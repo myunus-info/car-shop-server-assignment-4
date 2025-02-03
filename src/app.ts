f99757e12384b4bb93c6e1b00efaf1c66a506cf1
Muhammad Yunus <myunus_info@yahoo.com>
2025-02-03 15:58:31 +0200

    Modified Origin to Live server Link

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://car-shop-client.vercel.app', credentials: true }));
 
 // logging
 app.use(morgan('dev'));
